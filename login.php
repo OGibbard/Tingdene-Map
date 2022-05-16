0ff0c1f170479b5fef2df123d54478cd45269ef9
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-05-16 10:41:15 +0100

    6

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <Title>
+            Login
+        </Title>    
+    </head>
+    <body>
+        <h1>
+            Please login.
+        </h1>
+        <form action='loginprocess.php' method='POST'>
+            User name:<input type='text' name='Username'><br>
+            Password:<input type='password' name='Pword'><br>
+            <input type="submit" value="Login"> 
+        </form>
+    </body>
+</html>
