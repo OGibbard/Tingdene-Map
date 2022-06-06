f2c390ba231f11205e9bb8afd6a931388940e61d
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-06-06 10:35:45 +0100

    16

---
+++ homepage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <Title>
+            Homepage
+        </Title>    
+    </head>
+    <body>
+        <a href="login.php">Click here to login.</a>
+        <br>
+        <a href="signup.php">Click here to sign up.</a>
+    </body>
+</html>
