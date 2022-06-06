33a51f2edbcd03f05850a8555a56fffac9478d69
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-06-06 09:58:37 +0100

    15

--- loginprocess.php
+++ loginprocess.php
@@ -2,9 +2,9 @@
 session_start();
 include_once ('connection.php');
 
-array_map('htmlspecialchars', $_POST)
+array_map('htmlspecialchars', $_POST);
 
-$stmt = $conn->prepare("SELECT * FROM Accounts WHERE =Username =:username ;" ); 
+$stmt = $conn->prepare("SELECT * FROM accounts WHERE Username =:username ;" ); 
 
 $stmt->bindParam(':username', $_POST['username']); 
 
@@ -19,20 +19,21 @@ while ($row= $stmt->fetch(PDO::FETCH_ASSOC))
 
         if($row['AccountType']=='user'){
             $_SESSION['accounttype']='user';
-            header('Location: homepage.php')
+            header('Location: homepage.php');
 
         }elseif($row['AccountType']=='customer'){
             $_SESSION['accounttype']='customer';
-            header('Location: homepage.php')
+            header('Location: homepage.php');
 
         }elseif($row['AccountType']=='admin'){
             $_SESSION['accounttype']='admin';
-            header('Location: homepage.php')
+            header('Location: homepage.php');
+        }
     }
 }
 if($_POST['passwd']=='')
 {
-    header('Location: login.php')
+    header('Location: login.php');
 }
 $conn=null;
 
