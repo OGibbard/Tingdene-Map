7d1cc0f4f4e7751fc68755100df9219d79c28ff7
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-06-14 10:40:26 +0100

    19

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 // Initialize and add the map
+const icons = {
+  park: {
+    icon: "TingdenePark.png",
+  },
+  marina: {
+    icon: "TingdeneMarina.png",
+  },
+};
+
+const thamesandkennet = { lat: 51.4623018, lng: -0.9519213 };
+
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
+    // The location of Thames and Kennet
+    // The map, centered at Thames and Kennet
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 15,
+      center: thamesandkennet,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at Thames and Kennet
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: thamesandkennet,
       map: map,
+      icon: marina,
     });
-  }
-  
-  window.initMap = initMap;
+}
+
+window.initMap = initMap;
   
--- map.php
+++ map.php
@@ -12,12 +12,6 @@
     <!--The div element for the map -->
     <div id="map"></div>
 
-    <!-- 
-     The `defer` attribute causes the callback to execute after the full HTML
-     document has been parsed. For non-blocking uses, avoiding race conditions,
-     and consistent behavior across browsers, consider loading using Promises
-     with https://www.npmjs.com/package/@googlemaps/js-api-loader.
-    -->
     <script async
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzpVcQynoGYjwPIJi4gAoIPvFBXI30J6w&callback=initMap&v=weekly"
     ></script>
