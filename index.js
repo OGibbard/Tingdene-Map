3bc65d846fb47c42a484390104f5b3608792de8b
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-06-23 10:22:46 +0100

    20

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// Initialize and add the map
-const icons = {
-  park: {
-    icon: "TingdenePark.png",
-  },
-  marina: {
-    icon: "TingdeneMarina.png",
-  },
-};
-
-const thamesandkennet = { lat: 51.4623018, lng: -0.9519213 };
+let map;
 
 function initMap() {
     // The location of Thames and Kennet
+    const thamesandkennet = { lat: 51.4623018, lng: -0.9519213 };
     // The map, centered at Thames and Kennet
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 15,
       center: thamesandkennet,
     });
+    
+    const parkicon = "TingdenePark.png";
+    const marinaicon = "TingdeneMarina.png";
+
     // The marker, positioned at Thames and Kennet
-    const marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
       position: thamesandkennet,
       map: map,
-      icon: marina,
+      icon: marinaicon,
     });
 }
 
