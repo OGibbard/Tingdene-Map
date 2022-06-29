dfb1ea22916f3121da2e0458f2b94368062e4770
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-06-29 12:48:37 +0100

    23

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 let map;
 
 function initMap() {
-    // The location of Thames and Kennet
-    const thamesandkennet = { lat: 51.4623018, lng: -0.9519213 };
     // The map, centered at Thames and Kennet
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: thamesandkennet,
+      zoom: 6,
+      center: {lat: 54.3760767, lng: -2.5588948},
     });
 
-    const marinapin = 'TingdeneMarina.png';
-    const parkpin = 'TingdenePark.png';
+    const marina = 'TingdeneMarina.png';
+    const park = 'TingdenePark.png';
 
-    const thamesandkennetmarker = new google.maps.Marker({
-      position: {lat: 51.4623018, lng: -0.9519213},
-      map: map,
-      icon: marinapin,
-    });
-    const osbornemarker = new google.maps.Marker({
-      position: {lat: 52.679472, lng: 0.157083},
-      map: map,
-      icon: parkpin,
-    })
+    const sites = [
+      // thamesandkennet
+      {
+        position: {lat: 51.4623018, lng: -0.9519213},
+        map: map,
+        type: marina,
+      },
+      // osborne
+      {
+        position: {lat: 52.679472, lng: 0.157083},
+        map: map,
+        type: park,
+      },
+    ];
+    
+    for (let i = 0; i < sites.length; i++) {
+      const marker = new google.maps.Marker({
+        position: sites[i].position,
+        icon: sites[i].type,
+        map: map,
+      })
+    }
 }
 
 window.initMap = initMap;
