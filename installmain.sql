8c7c78c5b74faa1774087e775f380c3bd8e77157
ogibbard <gibbard.o@oundleschool.org.uk>
2022-12-03 18:42:48 +0000

    71

--- installmain.sql
+++ installmain.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2022 at 12:18 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Dec 03, 2022 at 07:41 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,18 +31,19 @@ CREATE TABLE `accounts` (
   `Username` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
   `AccountType` varchar(20) NOT NULL,
-  `Company` varchar(1000) NOT NULL
+  `Company` varchar(1000) NOT NULL,
+  `Tier` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`Username`, `Password`, `AccountType`, `Company`) VALUES
-('ethan', 'zone', 'user', 'Tingdene'),
-('gibby', 'admin', 'admin', 'Tingdene'),
-('johnsmith', '%)Ea#rK85V]d_Vm\"', 'user', ''),
-('oliver', 'oliver', 'user', '');
+INSERT INTO `accounts` (`Username`, `Password`, `AccountType`, `Company`, `Tier`) VALUES
+('ethan', '543e33c48b3c23d3b3ef151358533bc206fa0225ba66b89933a795451f016479', 'user', 'Tingdene', 2),
+('gibby', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', 'Tingdene', 3),
+('johnsmith', '1676fc26dc9117da77de3f8df83dcc19b9ae4e31fcbcacd0eb2624271f32d260', 'user', '', 1),
+('oliver', '7dfef7aed2105b7eceb4d34e1ad84fdad4693bd5de041e1b47079efeb6001a83', 'user', 'Tingdene', 1);
 
 -- --------------------------------------------------------
 
@@ -57,17 +58,21 @@ CREATE TABLE `properties` (
   `Latitude` varchar(15) NOT NULL,
   `Longitude` varchar(15) NOT NULL,
   `WebsiteLink` varchar(1000) NOT NULL,
-  `Company` varchar(100) NOT NULL
+  `Company` varchar(100) NOT NULL,
+  `Area` varchar(20) NOT NULL,
+  `Valuation` varchar(20) NOT NULL,
+  `Capacity` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `properties`
 --
 
-INSERT INTO `properties` (`SiteID`, `SiteName`, `SiteType`, `Latitude`, `Longitude`, `WebsiteLink`, `Company`) VALUES
-(2, 'Windsor', 'Marina', '51.4879317', '-0.6381952', 'Windsor', 'Tingdene'),
-(3, 'Thames and Kennet', 'Marina', '51.4623018', '-0.9519213', 'Thames-and-Kennet', 'Tingdene'),
-(4, 'Osborne', 'Park', '52.679472', '0.157083', 'Osborne', 'Tingdene');
+INSERT INTO `properties` (`SiteID`, `SiteName`, `SiteType`, `Latitude`, `Longitude`, `WebsiteLink`, `Company`, `Area`, `Valuation`, `Capacity`) VALUES
+(23, 'Thames and Kennet', 'Marina', '51.4623018', '-0.9519213', 'Thames-and-Kennet', 'Tingdene', '0', '0', '0'),
+(26, 'Osborne', 'Park', '52.679472', '0.157083', 'Osborne', 'Tingdene', '0', '0', '0'),
+(27, 'Windsor', 'Marina', '51.4879317', '-0.6381952', 'Windsor', 'Tingdene', '0', '0', '0'),
+(28, 'Badgerwood', 'Park', '51.389631', '-0.787389', 'Badgerwood', 'Tingdene', '0', '0', '0');
 
 --
 -- Indexes for dumped tables
@@ -93,7 +98,7 @@ ALTER TABLE `properties`
 -- AUTO_INCREMENT for table `properties`
 --
 ALTER TABLE `properties`
-  MODIFY `SiteID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `SiteID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- map.php
+++ map.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['name'])==false){
     <title>Add Map</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <link rel="stylesheet" type="text/css" href="./gmaps.css" />
-    
+    <link rel='stylesheet' href='backarrow.css'>
     <?php
     include_once("connection.php");
     array_map("htmlspecialchars", $_POST);
@@ -21,29 +21,17 @@ if (isset($_SESSION['name'])==false){
     $test = $stmt->fetchAll();
     $properties = json_encode($test);
     ?>
-
     <script>
     var properties = (<?php echo $properties;?>);
     </script>
-
     <script type="module" src="./index.js"></script>
-
   </head>
   <body>
-    <a><?php echo $_SESSION['name']?><a><br>
-    <a href='homepage.php'>Click here to return to the homepage.</a>
-    <a id="adminshow" href="admin.php">Click here for the admin page.</a>
-        <script type="text/javascript">
-            if("<?php echo $_SESSION['accounttype']; ?>" === 'admin'){
-                document.getElementById("adminshow").style.display = "block";
-            }else{
-                document.getElementById("adminshow").style.display = "none";
-            }
-        </script>
-    <h3>My Google Maps Demo</h3>
+    <a href="homepage.php" class="arrow previous">&#8249;</a>
+    <p align='center'><?php echo $_SESSION['name']?> - <?php echo $_SESSION['company']?></p><br>
+
     <!--The div element for the map -->
     <div id="map"></div>
-
     <script async
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzpVcQynoGYjwPIJi4gAoIPvFBXI30J6w&callback=initMap&v=weekly"
     ></script>
